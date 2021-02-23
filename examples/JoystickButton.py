868a441bc17019861a82c395ad29d6f27d9eafe4
KIU Shueng Chuan <nixchuan@gmail.com>
2021-02-23 18:16:10 +0800

    try to fix JoystickButton.py

--- examples/JoystickButton.py
+++ examples/JoystickButton.py
@@ -8,49 +8,50 @@ letting go of the joystick).
 
 import initExample ## Add path to library (just for examples; you do not need this)
 
-from pyqtgraph.Qt import QtGui, QtCore
+from pyqtgraph.Qt import QtCore, QtWidgets
 import pyqtgraph as pg
 
+class MainWindow(QtWidgets.QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        cw = QtWidgets.QWidget()
+        layout = QtWidgets.QGridLayout()
+        cw.setLayout(layout)
+        self.setCentralWidget(cw)
+        self.resize(300,50)
+
+        self.l1 = pg.ValueLabel(siPrefix=True, suffix='m')
+        self.l2 = pg.ValueLabel(siPrefix=True, suffix='m')
+        self.jb = pg.JoystickButton()
+        self.jb.setFixedWidth(30)
+        self.jb.setFixedHeight(30)
+
+        layout.addWidget(self.l1, 0, 0)
+        layout.addWidget(self.l2, 0, 1)
+        layout.addWidget(self.jb, 0, 2)
+
+        self.x = 0
+        self.y = 0
+
+        self.timer = QtCore.QTimer()
+        self.timer.timeout.connect(self.update)
+        self.timer.start(30)
+
+    def update(self):
+        dx, dy = self.jb.getState()
+        self.x += dx * 1e-3
+        self.y += dy * 1e-3
+        self.l1.setValue(self.x)
+        self.l2.setValue(self.y)
 
 app = pg.mkQApp("Joystick Button Example")
-mw = QtGui.QMainWindow()
-mw.resize(300,50)
-mw.setWindowTitle('pyqtgraph example: JoystickButton')
-cw = QtGui.QWidget()
-mw.setCentralWidget(cw)
-layout = QtGui.QGridLayout()
-cw.setLayout(layout)
+mw = MainWindow()
 mw.show()
-
-l1 = pg.ValueLabel(siPrefix=True, suffix='m')
-l2 = pg.ValueLabel(siPrefix=True, suffix='m')
-jb = pg.JoystickButton()
-jb.setFixedWidth(30)
-jb.setFixedHeight(30)
-
-
-layout.addWidget(l1, 0, 0)
-layout.addWidget(l2, 0, 1)
-layout.addWidget(jb, 0, 2)
-
-x = 0
-y = 0
-def update():
-    global x, y, l1, l2, jb
-    dx, dy = jb.getState()
-    x += dx * 1e-3
-    y += dy * 1e-3
-    l1.setValue(x)
-    l2.setValue(y)
-timer = QtCore.QTimer()
-timer.timeout.connect(update)
-timer.start(30)
-    
-    
 
 
 ## Start Qt event loop unless running in interactive mode or using pyside.
 if __name__ == '__main__':
     import sys
     if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
-        QtGui.QApplication.instance().exec_()
+        QtWidgets.QApplication.instance().exec_()

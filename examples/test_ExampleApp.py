d94d1fcaaaec008fb3531a5e56df1ec02730debc
Ogi Moore <ognyan.moore@gmail.com>
2021-05-28 08:44:23 -0700

    Close the example app after it launches

--- examples/test_ExampleApp.py
+++ examples/test_ExampleApp.py
@@ -1,11 +1,18 @@
 import initExample ## Add path to library (just for examples; you do not need this)
 
 import pyqtgraph as pg
-from pyqtgraph.Qt import QtGui, QtCore
+from pyqtgraph.Qt import QtGui, QtCore, mkQApp, QtTest
+
 
 from examples.ExampleApp import ExampleLoader
 
-loader = ExampleLoader()
+def test_ExampleLoader():
+    app = pg.mkQApp()
+    loader = ExampleLoader()
+    QtTest.QTest.qWaitForWindowExposed(loader)
+    QtTest.QTest.qWait(200)
+    loader.close()
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    test_ExampleLoader()
     pg.exec()

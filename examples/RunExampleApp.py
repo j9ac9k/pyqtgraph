eac6683e7471a653a2c9d5e51f2cfe4741564484
Ogi Moore <ognyan.moore@gmail.com>
2021-05-28 23:29:35 -0700

    Rename test_ExampleApp.py to RunExampleApp.py
    This change is done so that pytest does not run the example app twice,
    which I think has been the cause of some of the timeout errors we have
    been seeing in the CI systems.

--- examples/RunExampleApp.py
+++ examples/RunExampleApp.py
@@ -4,6 +4,13 @@ import pyqtgraph as pg
 from pyqtgraph.Qt import QtTest
 from examples.ExampleApp import ExampleLoader
 
+"""
+This file is used by test_examples.py for ensuring the Example App works.
+It is not named test_ExampleApp.py as that way the Example application is
+not run twice.
+"""
+
+
 pg.mkQApp()
 
 def test_ExampleLoader():

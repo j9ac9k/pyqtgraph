337fa523501abf2b66849244b4b1955325aeebc4
Ogi Moore <ognyan.moore@gmail.com>
2021-05-28 21:15:11 -0700

    Replace uses of tempfile.mktemp with tempfile.NamedTemporaryFile
    This issue was flagged as a security issue in the static code analyzer

--- tests/test_configparser.py
+++ tests/test_configparser.py
@@ -6,31 +6,24 @@ def test_longArrays():
     """
     Test config saving and loading of long arrays.
     """
-    tmp = tempfile.mktemp(".cfg")
-
     arr = np.arange(20)
-    configfile.writeConfigFile({'arr':arr}, tmp)
-    config = configfile.readConfigFile(tmp)
-    
+    with tempfile.NamedTemporaryFile(suffix=".cfg") as f:
+        configfile.writeConfigFile({'arr': arr}, f.name)
+        config = configfile.readConfigFile(f.name)    
     assert all(config['arr'] == arr)
-
-    os.remove(tmp)
 
 def test_multipleParameters():
     """
     Test config saving and loading of multiple parameters.
     """
-    tmp = tempfile.mktemp(".cfg")
-
     par1 = [1,2,3]
     par2 = "Test"
     par3 = {'a':3,'b':'c'}
 
-    configfile.writeConfigFile({'par1':par1, 'par2':par2, 'par3':par3}, tmp)
-    config = configfile.readConfigFile(tmp)
+    with tempfile.NamedTemporaryFile(suffix=".cfg") as f:
+        configfile.writeConfigFile({'par1':par1, 'par2':par2, 'par3':par3}, f.name)
+        config = configfile.readConfigFile(f.name)
     
     assert config['par1'] == par1
     assert config['par2'] == par2
     assert config['par3'] == par3
-
-    os.remove(tmp)
